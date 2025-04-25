a47f9c9bb3123b3cd0c1746afce8528d80d80f91
Paul Bessell <prbessell@gmail.com>
2025-04-25 11:06:58 +0100

    Oncho plots

---
+++ scripts/oncho/ALL_IU_Plots_oncho_Export.R
@@ -0,0 +1,157 @@
+
+library(tidyverse)
+library(readr)
+library(ggplot2)
+library(Hmisc)
+
+setwd(".../IU_Plots") # Update
+plotOutputPath <- paste0("Oncho/Plots/", Sys.Date(), "/Individual")
+outpath <- if(!dir.exists(plotOutputPath)) dir.create(plotOutputPath, recursive = T)
+plotOutputPath <- paste0("Oncho/Plots/", Sys.Date())
+
+ius <- read_csv("Data/Oncho/IU_List.csv") # Necessary for curating IU names later. Just a global list
+
+prevPath <- "Oncho/epioncho-MTP-v0-20241211-disaggregated-projections-prevalence-data/epioncho-MTP-v0-20241211-disaggregated/epioncho-MTP-v0-20241211-disaggregated"
+prevalenceFiles <- list.files(prevPath) # Requires updating with folder names. Also below
+iusList <- gsub(".csv|output_full_MTP_", "", prevalenceFiles)
+
+geostat <- get(load("Data/Oncho/ALL_prevalence_map_multipletimespoints.rds"))
+
+geostatcomb <- as.data.frame(geostat[[1]]) %>%
+  mutate(Year = 2002) %>%
+  bind_rows(as.data.frame(geostat[[2]]) %>%
+              mutate(Year = 2013))  %>%
+  bind_rows(as.data.frame(geostat[[3]]) %>%
+              mutate(Year = 2022))
+# names(geostatcomb) <- gsub("data.", "", names(geostatcomb))
+
+geostatcombGather <- geostatcomb %>%
+  dplyr::select(-TaskID) %>%
+  gather(key = "SimID", value = "Prev", -c("Year", "IU_CODE")) %>%
+  mutate(SimID = gsub("V", "X", SimID)) %>%
+  left_join(ius %>%
+              dplyr::select(IU_ID, IU_CODE, IU_ID_Full)) %>%
+  filter(!is.na(Prev))
+
+onchohistory <- read_csv("Data/Oncho/final_df_oncho_Parent_Child.csv") %>% # IU history data
+  mutate(IU_ID_Full = ifelse(IU_ID_MAPPING < 10000, paste0(ADMIN0ISO3, "0", IU_ID_MAPPING), paste0(ADMIN0ISO3, IU_ID_MAPPING)))
+
+onchoSites <- read_csv("Data/Oncho/Oncho_Sites.csv") %>% # Site level data from ESPEN
+  mutate(IU_ID_Full = ifelse(IU_2021 < 10000, paste0(ISO3, "0", IU_2021), paste0(ISO3, IU_2021))) %>%
+  filter(Examined >= Positive) %>%
+  mutate(monthN = match(SurveyMonth, month.name),
+         xPos = SurveyYear + ((monthN - 6) / 13) +
+           runif(nrow(.), -0.3, 0.3)) # %>%
+  # left_join(ius, by = c("IU_2021" = "IU_ID"))
+
+  #        sampleType = NA,
+  #        sampleType = ifelse(Method_2 %in% c("Blood smear", "Filtration",  "Chamber"), "mf", sampleType),
+  #        sampleType = ifelse(Method_2 %in% c("FTS (Ag)", "ICT (Ag)"), "Ag", sampleType)
+  # ) %>%
+  # filter(!is.na(sampleType)) %>%
+  # filter(!is.na(Examined), !is.na(Positive))
+
+# lfRegions <- read_csv("Data/LF/LF_Regions/IU_regions.csv")
+
+mda <- read_csv("Data/Oncho/Full_histories_df_popinfo_210324.csv")
+
+    
+helperDF <- line_df <- data.frame( # Needed for plotting
+  x = seq(2000, 2020, 5),
+  y = rep(-0.02, 5),
+  xend = seq(2000, 2020, 5),
+  yend = rep(0,5)
+)
+
+pdf(file = paste0(plotOutputPath, "/Oncho_IU_Plots.pdf"),onefile = TRUE)
+for(i in 1:length(iusList)){
+# for(i in 1:100){
+    cIU <- iusList[i]
+
+    exprev <- read_csv(paste0(prevPath, "/output_full_MTP_", cIU, ".csv")) %>%
+      filter(measure == "prevalence",
+              year_id >= 2000) %>%
+      dplyr::select(-c(age_start, age_end, measure)) %>%
+      gather(key = "Sim_ID", value = "Prevalence", -c("year_id"))
+  
+    exgeostat <- geostatcombGather %>%
+      filter(IU_CODE == cIU)
+    exoncho <- onchohistory %>%
+      filter(IU_CODE == cIU)
+    exmda <- mda %>%
+      filter(IU_CODE_MAPPING == cIU) %>%
+      filter(Year >= 2000)
+    
+    sitePresent <- cIU %in% onchoSites$IU_CODE # Check whether there is site-level data
+    if(sitePresent){
+      exsites <- onchoSites %>%
+        filter(IU_CODE == cIU,
+               Examined >= Positive) %>%
+        mutate(mprev = binconf(Positive, Examined)[,1],
+               lci = binconf(Positive, Examined)[,2],
+               uci = binconf(Positive, Examined)[,3])
+      }
+
+    max1 <- max(c(exprev$Prevalence)) # Setr the limits for the axes
+    if(sitePresent) max1 <- max(c(max1, exsites$uci))
+    
+    plottop <- ceiling(max1 / 0.1) * 0.1
+    plotbottom <- -0.2 * plottop
+    helperDF$y2 <- helperDF$y * plottop
+    
+    tester <- sum(!is.na(exoncho$Endemicity_P)) != 0
+    
+      if(tester){
+        lastyear <- max(exoncho$Year[!is.na(exoncho$Parent1_IU_ID)])
+        parentIU <- exoncho$Parent1_IU_ID[exoncho$Year == lastyear]
+        lastIU <- ifelse(parentIU < 10000, paste0(exoncho$ADMIN0ISO3[exoncho$Year == 2021], "0", parentIU), paste0(exoncho$ADMIN0ISO3[exoncho$Year == 2021], parentIU))
+        allIUs <- unique(onchohistory$IU_ID_Full[onchohistory$Parent1_IU_ID == parentIU])
+        allIUs <- paste(allIUs[!is.na(allIUs)], collapse = ", ")
+      }
+    
+    
+    testPlot <- ggplot(data = exgeostat, aes(x = Year, y = Prev, group = Year)) + 
+      geom_line(data = exprev, aes(x = year_id, y = Prevalence, group = Sim_ID), col = "grey85") + theme(axis.title.x = element_blank()) +
+      geom_violin(fill = "#057AC1", col = "#057AC1", alpha = 0.8, width = 1) +
+      geom_segment(x = 2000, xend = 2023, y = -0.1 * plottop, yend = -0.1 * plottop, size = 10, col = NA)
+    
+    if(nrow(exmda) > 0) {
+      testPlot <- testPlot +  
+      geom_point(data = exmda, aes(x = Year, y = -.1 * plottop), inherit.aes = F, shape = 2, size = 2)
+    }
+    
+    if(sitePresent){
+      testPlot <- testPlot + 
+        geom_point(data = exsites, aes(x = xPos, y = mprev), col = "#DE4800", inherit.aes = F) +
+        geom_linerange(data = exsites, aes(x = xPos, ymin = lci, ymax = uci), col = "#DE4800", inherit.aes = F)
+    }
+    
+    
+    testPlot <- testPlot +
+      scale_x_continuous(limits = c(1999.5, 2024)) +
+      scale_y_continuous(limits = c(plotbottom, plottop)) +
+      theme_bw() +
+      theme(axis.title.x = element_blank(),
+            axis.ticks.x = element_blank(),
+            panel.grid = element_blank())  +
+      theme(axis.line.x = element_blank(), axis.text.x = element_blank()) +
+      geom_hline(yintercept = 0) +
+      geom_segment(data = helperDF, aes(x = x, xend = xend, y = y2, yend = yend), inherit.aes = F) +
+      geom_text(data = helperDF, aes(x = x, y = (y2- (0.02 * plottop)), label = x), inherit.aes = F) +
+      ylab("Prevalence")
+    
+    if(tester){
+      testPlot <- testPlot +
+      geom_text(aes(x = mean(c(2000, lastyear)), y = -0.15 * plottop), label = lastIU) +
+      geom_text(aes(x = mean(c(2000, lastyear)), y = -0.2 * plottop), label = paste0("(", allIUs, ")"), size = 3) +
+      geom_text(aes(x = mean(c(2023, lastyear)), y = -0.175 * plottop), label = cIU)
+    }
+    if(!tester) testPlot <- testPlot +  geom_text(aes(x = mean(c(2000, 2024)), y = -0.2 * plottop), label = cIU)
+      testPlot <- testPlot +  geom_hline(yintercept = 0.01, lty = "dashed")
+    
+    
+    print(testPlot)
+    ggsave(testPlot, file = paste0(plotOutputPath, "/Individual/", cIU, ".jpg"))
+
+}
+dev.off()
